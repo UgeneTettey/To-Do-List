04bed8bf6110f9a9048a52ec0f83e82ac85c49ee
UgeneTettey <ugeneayerkain@gmail.com>
2024-05-15 19:09:15 +0000

    categorized bmi, added readme file

--- BMI-calculator.py
+++ BMI-calculator.py
@@ -16,4 +16,18 @@ bmi = get_bmi()
 print(f'Your BMI is {bmi}')
 
 # add lines to convert bmi in cases where weight and height are gven in other units
-# categorize the bmi to fit
+
+
+# categorize the bmi to determine weight class of user
+if bmi < 18.5:
+    print('You are underweight')
+elif 18.5 < bmi <= 24.9:
+    print('You have a normal weight')
+elif 25 <= bmi <= 29.9:
+    print('You are overweight')
+elif bmi <= 30 and bmi <= 34.9:
+    print('Obesity Class I: Moderate; Watch out!')
+elif bmi >= 35 and bmi <= 39.9:
+    print('Obesity Class II: Severe')
+else:
+    print('Obesity Class III: Morbid Obesity!')
